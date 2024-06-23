95f3bc72b1a2b9fb09450204f02b97b856a6dcf3
zili11720 <zili11720@gmail.com>
2024-06-23 19:29:03 +0300

    add multiple locations to map

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -23,6 +23,10 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
@@ -80,7 +84,8 @@ public class MapFragment extends Fragment {
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
                             Log.d(TAG, "Latitude: " + latitude + ", Longitude: " + longitude);
-                            loadMapWithLocation(latitude, longitude);
+                            List<double[]> locations = generateRandomLocations(latitude, longitude, 5); // Generate 5 random locations
+                            loadMapWithLocation(latitude, longitude, locations);
                         } else {
                             Log.d(TAG, "Location is null");
                         }
@@ -88,8 +93,19 @@ public class MapFragment extends Fragment {
                 });
     }
 
+    private List<double[]> generateRandomLocations(double latitude, double longitude, int count) {
+        List<double[]> locations = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            double latOffset = (random.nextDouble() - 0.5) / 1000; // random offset in the range of ±0.0005
+            double lonOffset = (random.nextDouble() - 0.5) / 1000; // random offset in the range of ±0.0005
+            locations.add(new double[]{latitude + latOffset, longitude + lonOffset});
+        }
+        return locations;
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
-    private void loadMapWithLocation(double latitude, double longitude) {
+    private void loadMapWithLocation(double latitude, double longitude, List<double[]> locations) {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
@@ -97,7 +113,7 @@ public class MapFragment extends Fragment {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 Log.d(TAG, "WebView loaded URL: " + url);
-                String jsCode = "javascript:updateMapLocation(" + latitude + ", " + longitude + ")";
+                String jsCode = "javascript:updateMapLocation(" + latitude + ", " + longitude + ", " + locationsToJson(locations) + ")";
                 webView.evaluateJavascript(jsCode, null);
             }
 
@@ -108,6 +124,19 @@ public class MapFragment extends Fragment {
         });
         webView.loadUrl("file:///android_asset/leaflet_map.html");
         Log.d(TAG, "Loading URL: file:///android_asset/leaflet_map.html");
+    }
+
+    private String locationsToJson(List<double[]> locations) {
+        StringBuilder json = new StringBuilder("[");
+        for (int i = 0; i < locations.size(); i++) {
+            double[] loc = locations.get(i);
+            json.append("[").append(loc[0]).append(", ").append(loc[1]).append("]");
+            if (i < locations.size() - 1) {
+                json.append(", ");
+            }
+        }
+        json.append("]");
+        return json.toString();
     }
 
     @Override
